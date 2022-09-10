2aaa78b82820597bc2764d1edb27d6c0215213c9
Gabriel Santos <gabriel.santos@fluencypass.com>
2022-09-10 14:10:57 -0300

    Adding Container Component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+import * as S from './styles'
+
+type ContainerTypes = {
+  children: ReactNode
+}
+
+export default function Container({ children }: ContainerTypes) {
+  return <S.Container data-testid="container">{children}</S.Container>
+}
