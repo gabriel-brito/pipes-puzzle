823870ebfe36a929eec2e9322f89c419c1686016
Gabriel Santos <gabriel.santos@fluencypass.com>
2022-09-11 12:53:36 -0300

    Updating types

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,9 +1,7 @@
-import { ReactNode } from 'react'
-
 import * as S from './styles'
 
 type ContainerTypes = {
-  children: ReactNode
+  children: JSX.Element | JSX.Element[]
 }
 
 export default function Container({ children }: ContainerTypes) {
